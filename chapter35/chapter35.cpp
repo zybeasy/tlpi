6588dc438da1a5b6c250ef40ca292bb2c106ac65
zybeasy <zybeasy@163.com>
2020-06-05 01:43:10 +0800

    chapter 35

--- chapter35/chapter35.cpp
+++ chapter35/chapter35.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <sched.h>
 #include <sys/resource.h>
 
 #include <unistd.h>
@@ -11,4 +12,13 @@ using namespace std;
 
 int chapter35(int argc, char* argv[]) {
     cout << "PID=" << getpid() << " NICE=" << getpriority(PRIO_PROCESS, 0) << endl;
+
+    cout << SCHED_RR << " " << SCHED_BATCH << endl;
+
+    cout << sched_get_priority_min(SCHED_RR) << " " << sched_get_priority_max(SCHED_RR) << endl;
+
+    for(int i = 1; i < 10; ++i)
+        cout << sched_getscheduler(i) << " " << i << " XXXXXXXX" << endl;
+
+    return 0;
 }
